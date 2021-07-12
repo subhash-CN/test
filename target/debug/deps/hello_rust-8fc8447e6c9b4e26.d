666a37069f82d5a18ec3a98326972775727ce88a
Subhash Suman <subhash.suman@codenation.co.in>
2021-07-12 23:31:56 +0530

    done 2

--- target/debug/deps/hello_rust-8fc8447e6c9b4e26.d
+++ target/debug/deps/hello_rust-8fc8447e6c9b4e26.d
@@ -1,6 +1,6 @@
-/home/subhashsuman01/Desktop/hello-rust/target/debug/deps/hello_rust-8fc8447e6c9b4e26.rmeta: src/main.rs
+/home/subhashsuman01/Desktop/test/target/debug/deps/hello_rust-8fc8447e6c9b4e26.rmeta: src/main.rs
 
-/home/subhashsuman01/Desktop/hello-rust/target/debug/deps/hello_rust-8fc8447e6c9b4e26.d: src/main.rs
+/home/subhashsuman01/Desktop/test/target/debug/deps/hello_rust-8fc8447e6c9b4e26.d: src/main.rs
 
 src/main.rs:
 
